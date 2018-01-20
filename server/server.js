31383bf72aca3d2e08476329f31887d83abc4b6d
codingblazer <sasachin04@gmail.com>
2018-01-21 03:15:58 +0530

    Adds sockets basic and connection event

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+io.on('connection',(socket)=>{
+  console.log('The connection event has occured');
+  socket.on('disconnect',()=>{
+    console.log('disconnect event');
+  });
+});
+
+
+
+
+server.listen(port,()=>{
   console.log('server is up on port :'+port);
 });
