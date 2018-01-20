6bb32f8c683ee80e84590bb157dd2b0d47d4f421
codingblazer <sasachin04@gmail.com>
2018-01-20 15:41:03 +0530

    Adds express basic server and gitignore

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const path = require('path');
-
-console.log(__dirname+'/../public');
+const express = require('express');
 
 const publicPath = path.join(__dirname,'../public');
-console.log(publicPath);
+
+var app = express();
+app.use(express.static(publicPath));
+
+app.listen(3000);
